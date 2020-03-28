9691b90ea248b0a54e78f11272f35996a8a7dd94
vikzelikov <vikzelikov@yandex.ru>
2020-03-28 03:44:44 +0300

    add logout in profile

--- app/src/main/java/bonch/dev/presenter/profile/ProfileDetailPresenter.kt
+++ app/src/main/java/bonch/dev/presenter/profile/ProfileDetailPresenter.kt
@@ -1,6 +1,5 @@
 package bonch.dev.presenter.profile
 
-import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceManager
 import bonch.dev.utils.Constants
 import bonch.dev.view.profile.ProfileDetailView
@@ -11,9 +10,6 @@ class ProfileDetailPresenter(val profileDetailView: ProfileDetailView) {
         val activity = profileDetailView
         val pref = PreferenceManager.getDefaultSharedPreferences(activity.applicationContext)
         val editor = pref.edit()
-
-        val ed =  profileDetailView.getPreferences(AppCompatActivity.MODE_PRIVATE)
-        println(ed.contains(Constants.ACCESS_TOKEN))
 
         editor.remove(Constants.ACCESS_TOKEN)
         editor.apply()
